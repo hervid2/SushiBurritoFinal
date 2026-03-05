087e7b0d84232b6f9144bba0b323cb5b506a6bd4
Stephany <stephanymoreno1826@gmail.com>
2026-03-04 22:47:53 -0500

    estilos importados a app.js

--- Frontend/src/app.js
+++ Frontend/src/app.js
@@ -1,9 +1,6 @@
 // Punto de entrada principal de la aplicación.
 
-// Importa el CSS principal para que Vite lo procese.
-import './assets/styles/main.css';
-
 // Importa todas las funcionalidades del router.
-import * as router from './router/router';
+import * as router from './router/router.js';
 
 console.log('App.js cargado. El router se encargará de la inicialización de la aplicación.');
